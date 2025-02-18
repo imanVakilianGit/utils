e6def51acf06dabd29614170c6ce7245579bdcec
imanvakilian <imanvakiliangitu@gmail.com>
2025-02-18 14:08:21 +0330

    feat(quick sort(performance approach) - javascript)

--- js/quick.sort.js
+++ js/quick.sort.js
@@ -4,7 +4,7 @@ const input = [3, 4, 1, 7, 33, 2, 22, 21, 66, 55];
 
 console.log("Unsorted input: ", input);
 
-function recursiveFunc(inputArr) {
+function readableApproach(inputArr) {
     if (inputArr.length < 2) return inputArr;
 
     const pivot = inputArr.at(-1);
@@ -16,10 +16,47 @@ function recursiveFunc(inputArr) {
         else leftSide.push(inputArr[i]);
     }
 
-    leftSide = recursiveFunc(leftSide);
-    rightSide = recursiveFunc(rightSide);
+    leftSide = readableApproach(leftSide);
+    rightSide = readableApproach(rightSide);
 
     return Array().concat(leftSide, [pivot], rightSide);
 }
 
-console.log("Sorted input: ", recursiveFunc(input));
+// console.log("Sorted input: ", readableApproach(input));
+
+// ============================= performance approach =========================================
+
+function performanceApproachHelper(inputArr, low, high) {
+    const pivot = inputArr.at(high);
+
+    let index = low - 1;
+
+    for (let i = low; i < high; i++) {
+        if (inputArr[i] <= pivot) {
+            index += 1;
+            [inputArr[i], inputArr[index]] = [inputArr[index], inputArr[i]];
+        }
+    }
+
+    [inputArr[high], inputArr[index + 1]] = [
+        inputArr[index + 1],
+        inputArr[high],
+    ];
+
+    return index + 1;
+}
+
+function performanceApproach(inputArr, low = 0, high) {
+    if (high === undefined) high = inputArr.length - 1;
+
+    if (low < high) {
+        const result = performanceApproachHelper(inputArr, low, high);
+
+        performanceApproach(inputArr, low, result - 1);
+        performanceApproach(inputArr, result + 1, high);
+    }
+
+    return inputArr;
+}
+
+console.log("Sorted input: ", performanceApproach(input));
